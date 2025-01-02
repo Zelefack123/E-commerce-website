cb288e04214f6091e824757d4e6e9f78a15ee24e
Zelefack123 <zelefackmarie858@gmail.com>
2025-01-02 20:12:43 +0100

    finished the design of a blog page

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.querySelector(".fa-bars");
   const cross = document.querySelector(".fa-xmark");
   const navLinks = document.querySelector(".navlinks");
+  const mainImage = document.querySelector("#mainImage");
+  const smallImages = document.querySelectorAll(".small-img-col");
+
 
   hamburger.onclick = () => {
+    alert("hamburger has been clicked");
+    console.log("hamburger h");
     navLinks.classList.add("active");
   };
 
@@ -11,4 +16,24 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("Cross clicked");
   navLinks.classList.remove("active");
   };
+
+  
+
+smallImages.forEach(element => {
+  let Image =  element.querySelector("img");
+  console.log(Image.src);
+  element.addEventListener("click",()=>{
+  if(Image) {
+   mainImage.src = Image.src;
+  }
+  else{
+    console.log("there is no img tag");
+  }
+   }
+  )
+    
+  });
+
+
 });
+
